e94be2a336ef88922d06eccfec21f0b33aab5a0a
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-21 17:36:21 +0530

    set error handler

--- backend/app.js
+++ backend/app.js
@@ -35,10 +35,19 @@ db.once('open', () => {
 
 app.use('/', require('./routes/indexRouter'))
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status(404);
-    next(error);
-});
+    const error = new Error('Not found')
+    error.status(404)
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 const PORT = 5000
 
