4a1ca1c42cb133df604cbb60e957751120ee4c75
raavinduf <ravinduf@rocketmail.com>
2020-12-23 23:05:40 +0530

    added querying through dates functionality

--- backend/controllers/tempData.js
+++ backend/controllers/tempData.js
@@ -3,13 +3,34 @@ const sensor = require('../models/processedReading')
 
 
 const getData = async (req, res) => {
-    console.log(req)
+    
+    console.log(req.query)
+    const queryStatement = {
+        reading_type: 'temperature'
+    }
+    
+    if (req.query.start && req.query.end) {
+        queryStatement.date = { $gte : req.query.start, $lte : req.query.end }
+    }
+    else if (req.query.start){
+
+        queryStatement.date = { $gte : req.query.start}
+    }
+    else if (req.query.end) {
+        queryStatement.date = { $lte : req.query.end}
+    }
+    
+
+    console.log(queryStatement)
     try{
-        const tempData = await processedReading.find({reading_type: 'temperature'})
+        
+        const tempData = await processedReading.find(queryStatement)
+        console.log(tempData.length)
         res.status(200).json(tempData)
     }
     
     catch (err){
+        console.log(err)
         res.status(400).json({message: 'Failure'})
     }
 }
