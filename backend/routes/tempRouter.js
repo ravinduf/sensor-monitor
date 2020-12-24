00a027c473b38be82a5763e92f7ca571ad32a1f1
raavinduf <ravinduf@rocketmail.com>
2020-12-24 09:12:41 +0530

    updated

--- backend/routes/tempRouter.js
+++ backend/routes/tempRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const sensor = require('../models/sensor')
-const {getData , getTempAlerts} = require('../controllers/tempData')
+const {getData , getTempAlerts} = require('../controllers/tempDataController')
 
 
 router.get('/tempData', getData)
