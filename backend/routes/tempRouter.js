faec584b997c48b3c9ca5a775843a3d1ee434313
raavinduf <ravinduf@rocketmail.com>
2020-12-24 06:59:25 +0530

    renamed indexRouter to tempRouter

--- backend/routes/tempRouter.js
+++ backend/routes/tempRouter.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const sensor = require('../models/sensor')
-const {getData} = require('../controllers/tempData')
+const {getData , getTempAlerts} = require('../controllers/tempData')
 
 
 router.get('/tempData', getData)
-
-
+router.get('/tempAlerts', getTempAlerts)
 
 
 module.exports = router
