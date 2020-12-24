565f529acee59e1f74cdb0c214bfb9a1531a1420
raavinduf <ravinduf@rocketmail.com>
2020-12-24 09:11:19 +0530

    renamed tempData to tempDataController

--- backend/controllers/tempDataController.js
+++ backend/controllers/tempDataController.js
@@ -3,8 +3,7 @@ const sensor = require('../models/processedReading')
 
 
 const getData = async (req, res) => {
-    
-    console.log(req.query)
+
     const queryStatement = {
         reading_type: 'temperature'
     }
@@ -13,15 +12,14 @@ const getData = async (req, res) => {
         queryStatement.date = { $gte : req.query.start, $lte : req.query.end }
     }
     else if (req.query.start){
-
         queryStatement.date = { $gte : req.query.start}
     }
     else if (req.query.end) {
         queryStatement.date = { $lte : req.query.end}
     }
     
-
     console.log(queryStatement)
+    
     try{
         
         const tempData = await processedReading.find(queryStatement)
@@ -35,4 +33,15 @@ const getData = async (req, res) => {
     }
 }
 
-module.exports = {getData}
+const getTempAlerts = async (req, res) => {
+    
+    try {
+        const alerts = await processedReading.find({ alert : { alertStatus : true}})    
+        res.status(200).json(alerts)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({message: "failure"})
+    }
+}
+
+module.exports = {getData, getTempAlerts}
