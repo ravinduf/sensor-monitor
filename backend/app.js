0e98aba4457d58c5bb311f83948ac9e1179f6247
raavinduf <ravinduf@rocketmail.com>
2020-12-29 10:33:08 +0530

    created DBListener

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const env = require('dotenv')
-const processedReading = require('./models/processedReading')
-const TemperatureLimit = require('./operations/checkTemperatureLimit')
-const convertToDateObject = require('./operations/convertToDateObject')
 const { Result } = require('express-validator')
+const DBListener = require('./operations/DBListener')
 const notificationSend = require('./operations/notifications/notificationSender')
 const user = require('./models/user')
 env.config()
@@ -34,53 +32,7 @@ db.once('open', () => {
     console.log('conneted to monogdb')
 
     const changeStream = sensorCollection.watch();
-
-    changeStream.on('change', (change) => {
-
-        if (change.operationType === 'insert') {
-            const data = change.fullDocument
-
-            const newProcessedReading = new processedReading({
-                sensor_reading_id: data._id,
-                sensor_id: data.sensor_id,
-                reading_type: 'temperature',
-                data_value: data.data_value,
-                date: convertToDateObject(data.date)
-            })
-
-            if (TemperatureLimit.checkValue(data.data_value)) {
-                // user.find()
-                //     .exec()
-                //     .then(result => {
-                //         notificationSend.notificationSender(result)
-                //     })
-                //     .catch(err => {
-                //         console.log("Can not send Notifications")
-                //     })
-                
-                newProcessedReading.alert = {
-                    alertStatus: true,
-                    alertText: "Temperature is greater than threshold value"
-                }
-            }
-
-            newProcessedReading
-                .save()
-                .then(result => {
-                    console.log(result)
-                })
-                .catch(err => {
-                    res.status(404).json({
-                        error: 'Error is occure'
-                    });
-                    console.log(err)
-                })
-
-
-            console.log("\n", data)
-        }
-
-    })
+    DBListener(changeStream);
 })
 
 app.use('/temperature', require('./routes/tempRouter'))
---
+++ backend/operations/DBListener.js
@@ -0,0 +1,56 @@
+const processedReading = require('../models/processedReading')
+const convertToDateObject = require('./convertToDateObject')
+const TemperatureLimit = require('./checkTemperatureLimit')
+const user = require('../models/user')
+const notificationSend = require('./notifications/notificationSender')
+
+const DBListener = (changeStream) => {
+
+    changeStream.on('change', (change) => {
+
+        if (change.operationType === 'insert') {
+            const data = change.fullDocument
+            console.log('dbListener')
+            const newProcessedReading = new processedReading({
+                sensor_reading_id: data._id,
+                sensor_id: data.sensor_id,
+                reading_type: 'temperature',
+                data_value: data.data_value,
+                date: convertToDateObject(data.date)
+            })
+
+            if (TemperatureLimit.checkValue(data.data_value)) {
+                user.find()
+                    .exec()
+                    .then(result => {
+                        console.log(result)
+                        notificationSend.notificationSender(result)
+                    })
+                    .catch(err => {
+                        console.log("Can not send Notifications")
+                    })
+                
+                newProcessedReading.alert = {
+                    alertStatus: true,
+                    alertText: "Temperature is greater than threshold value"
+                }
+            }
+
+            newProcessedReading
+                .save()
+                .then(result => {
+                    
+                })
+                .catch(err => {
+                    res.status(404).json({
+                        error: 'Error is occure'
+                    });
+                    console.log(err)
+                })
+
+            
+        }
+    })
+}
+
+module.exports = DBListener
