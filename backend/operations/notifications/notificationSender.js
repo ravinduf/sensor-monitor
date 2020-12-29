8610e6aef67ba385a18cbc21b39891ab48f729b6
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-29 12:45:09 +0530

    updated

--- backend/operations/notifications/notificationSender.js
+++ backend/operations/notifications/notificationSender.js
@@ -1,28 +1,20 @@
 const emailNotification = require('./notificationMethods/emailNotification')
 const smsNotification = require('./notificationMethods/smsNotification')
 
-// const notificationSender = ({notifier}) => {
-//     if(notifier.email){
-//         emailNotification();
-//     }
-
-//     else if (notifier.phoneNumber) {
-//         smsNotification();
-//         voiceNotification()
-//     }
-// }
-module.exports.notificationSender = function(notifier) {
-    if(notifier.email){
-        emailNotification()
-        return true
-    }
-
-    else if (notifier.phoneNumber) {
-        smsNotification()
-        voiceNotification()
-        return true
-    } else {
-        return false
-    }
+module.exports.notificationSender = function(notifiers) {
+    notifiers.forEach(function(notifier) {
+        if(notifier.email){
+            emailNotification()
+            return true
+        }
+    
+        else if (notifier.phoneNumber) {
+            smsNotification()
+            voiceNotification()
+            return true
+        } else {
+            return false
+        }
+    });
 }
 
