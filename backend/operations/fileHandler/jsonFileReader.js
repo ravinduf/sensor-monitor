23b1bf5c3ed02b512aa603dd55f2900fe6225e81
raavinduf <ravinduf@rocketmail.com>
2020-12-31 06:19:05 +0530

    fixed errors

--- backend/operations/fileHandler/jsonFileReader.js
+++ backend/operations/fileHandler/jsonFileReader.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
+const path = require('path')
+
+
 module.exports.readeFile = function(val) {
-    fs.readFile('../resources/sensors.json', 'utf8', (err, data) => {
-        if(err) {
-            console.error(err)
-            return
-        } else {
-            return data
-        }
-    })
+
+
+    try {
+        const data = fs.readFileSync('./operations/fileHandler/sensors.json', 'utf-8')
+        console.log(data)
+        return data
+    } catch (error) {
+        console.log       
+    }
 }
