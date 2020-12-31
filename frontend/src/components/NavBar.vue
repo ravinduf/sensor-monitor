a4cfd4e52d57c7f6d02895d2fa6fefda8dea6ac6
sampathdissanayaka <dmcsampath97@gmail.com>
2020-12-31 07:08:04 +0700

    update navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,12 +1,11 @@
 <template>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">SENSOR MONITOR</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarColor02">
+  <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home
@@ -14,15 +13,17 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Alert</a>
+        <a class="nav-link" href="#">Features</a>
       </li>
-      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
       <li class="nav-item">
         <a class="nav-link" href="#">About</a>
       </li>
-      
+     
     </ul>
-  
+   
   </div>
 </nav>
 </template>
